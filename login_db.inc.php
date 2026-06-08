657eec80e137904da550dcde46f046a6c46d6b3d
自由Kay <venancekayode86@gmail.com>
2026-06-08 10:45:47 +0100

    Change database user and password
    Updated database connection credentials for login.

--- login_db.inc.php
+++ login_db.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 $host = 'localhost';
-$user = '';
-$password = '';
+$user = 'Kayjiyu';
+$password = 'Venkay147';
 $db = 'innov_campus';
 
 $con = mysqli_connect($host, $user, $password, $db) or die("Message d'erreur : " . mysqli_connect_error());
